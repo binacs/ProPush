9cee8ae1c366cfe667e2774ded4de01131a456b6
BinacsLee <bin646891055@gmail.com>
2020-03-08 23:33:21 +0800

    metrics support PushMetrics to push to pushgateway

--- cmd/main.go
+++ cmd/main.go
@@ -9,4 +9,8 @@ import (
 func main() {
 	ins := collector.GetInstance()
 	fmt.Println(ins.GetMetrics())
+	err := ins.PushMetrics("http://127.0.0.1:9091", ins.GetMetrics())
+	if err != nil {
+		fmt.Println("PushMetrics err:", err)
+	}
 }
--- collector/interface.go
+++ collector/interface.go
@@ -1,16 +1,23 @@
 package collector
 
 import (
+	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/expfmt"
 )
 
 type Instance struct {
-	R *prometheus.Registry
-	C *NodeCollector
+	R        *prometheus.Registry
+	C        *NodeCollector
+	job      string
+	instance string
 }
 
 var instance *Instance
@@ -18,8 +25,10 @@ var instance *Instance
 func GetInstance() *Instance {
 	if instance == nil {
 		instance = &Instance{
-			R: prometheus.NewRegistry(),
-			C: NewNodeCollector(),
+			R:        prometheus.NewRegistry(),
+			C:        NewNodeCollector(),
+			job:      "defaultJobName",
+			instance: "defaultInstanceName",
 		}
 		if instance.R == nil || instance.C == nil || instance.R.Register(instance.C) != nil {
 			instance = nil
@@ -51,4 +60,47 @@ func (ins *Instance) GetMetrics() string {
 		}
 	}
 	return buf.String()
+}
+
+func (ins *Instance) PushMetrics(gateway string, data string) error {
+	sr := strings.NewReader(data)
+	br := bufio.NewReader(sr)
+	var url string
+	if gateway[len(gateway)-1] == '/' {
+		url = gateway + "metrics/job/" + ins.job + "/instance/" + ins.instance
+	} else {
+		url = gateway + "/metrics/job/" + ins.job + "/instance/" + ins.instance
+	}
+	req, err := http.NewRequest(http.MethodPost, url, br)
+	if err != nil {
+		return err
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusAccepted {
+		body, _ := ioutil.ReadAll(resp.Body)
+		errStr := fmt.Sprintf("unexpected status code %s, PushGateway url = %s, body = %s.", resp.StatusCode, url, string(body))
+		return errors.New(errStr)
+	}
+	return nil
+}
+
+func (ins *Instance) SetJob(job string) {
+	ins.job = job
+}
+
+func (ins *Instance) GetJob() string {
+	return ins.job
+}
+
+func (ins *Instance) SetInstance(instance string) {
+	ins.instance = instance
+}
+
+func (ins *Instance) GetInstance() string {
+	return ins.instance
 }
